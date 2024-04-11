e49f4c795d11c4db00f469ec8096bb581e72b1e2
David-11209 <limonchik087@gmail.com>
2024-04-11 13:02:51 +0300

    add MainScreen Files

--- Solfeggio/Solfeggio/CustomViews/GradientView.swift
+++ Solfeggio/Solfeggio/CustomViews/GradientView.swift
@@ -7,21 +7,14 @@ class GradientView: UIView {
         return CAGradientLayer.self
     }
 
-    init(frame: CGRect, color1: UIColor, color2: UIColor) {
+    override init(frame: CGRect) {
         super.init(frame: frame)
-        setUpGradient(color1: color1, color2: color2)
     }
-
-    init(frame: CGRect, color: UIColor) {
-        super.init(frame: frame)
-        setUpOneColor(color: color)
-    }
-
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
 
-    private func setUpGradient(color1: UIColor, color2: UIColor) {
+    func setUpGradient(color1: UIColor, color2: UIColor) {
         guard let gradientLayer = self.layer as? CAGradientLayer else { return }
         gradientLayer.colors = [color1.cgColor, color2.cgColor]
         gradientLayer.locations = [0.0, 1.0]
@@ -29,7 +22,7 @@ class GradientView: UIView {
         gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
     }
 
-    private func setUpOneColor(color: UIColor) {
+    func setUpOneColor(color: UIColor) {
         self.backgroundColor = color
     }
 }
---
+++ Solfeggio/Solfeggio/MainScreen/MainScreenSubCollectionView/MainScreenSubCollectionViewDataManager.swift
@@ -0,0 +1,64 @@
+//
+//  MainScreenSubCollectionViewDataManager.swift
+//  Solfeggio
+//
+//  Created by Давид Васильев on 08.04.2024.
+//
+
+import UIKit
+
+class MainScreenSubCollectionViewDataManager: NSObject, UICollectionViewDataSource {
+
+    private var data: [String]
+
+    init(data: [String]) {
+        self.data = data
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: MainScreenSubCollectionViewCell.reuseIdentifier, for: indexPath) as? MainScreenSubCollectionViewCell
+        else {
+            return UICollectionViewCell()
+        }
+        /// Это будет заменено,  после того как напишу модели
+        cell.configure(colors: cellDataArray.randomElement()?.colors ?? [.white, .white], title: "Нотная грамота", progressProcent: "57%")
+        return cell
+    }
+}
+
+private enum CellColor {
+    case red
+    case yellow
+    case mySPink
+    case myCyan
+    case green
+    case mySBlueLight
+    case myLightYellow
+    case mySPeach
+    case myBlue
+}
+
+private struct LevelCellColors {
+    var colors: [UIColor]
+}
+
+private var cellDataArray: [LevelCellColors] = [
+    LevelCellColors(colors: [.red, .yellow]),
+    LevelCellColors(colors: [.mySPink, .myCyan]),
+    LevelCellColors(colors: [.yellow, .green]),
+    LevelCellColors(colors: [.mySBlueLight, .myLightYellow]),
+    LevelCellColors(colors: [.mySPink, .mySPeach]),
+    LevelCellColors(colors: [.myBlue, .green]),
+    LevelCellColors(colors: [.red, .yellow]),
+    LevelCellColors(colors: [.mySPink, .myCyan]),
+    LevelCellColors(colors: [.yellow, .green]),
+    LevelCellColors(colors: [.mySBlueLight, .myLightYellow]),
+    LevelCellColors(colors: [.mySPink, .mySPeach]),
+    LevelCellColors(colors: [.myBlue, .green])
+]
+
